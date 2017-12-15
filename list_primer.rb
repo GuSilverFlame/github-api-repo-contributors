c1e75bed2e3a33a97ec2c802ada1e3074ad7a213
Gustavo Santos Ferreira <gustavosantosfe@gmail.com>
2017-12-15 09:33:20 +0000

    add class that sorts and expands the list

---
+++ list_primer.rb
@@ -0,0 +1,25 @@
+require 'json'
+class ListPrimer
+  attr_accessor :response
+  def initialize(response)
+    @response = response
+  end
+
+  def expand(url_col)
+    @list = @list.map do |result|
+      second_depth = JSON.parse(Request.new(result[url_col]).requesto)
+      result.merge(second_depth)
+    end
+  end
+
+  def parse
+    @list = JSON.parse(response)
+  end
+
+  def prime(order, expansible_column = nil)
+    parse
+    expand(expansible_column) if expansible_column
+    @list.sort{|first, second| second[order] <=> first[order]}
+  end
+
+end
